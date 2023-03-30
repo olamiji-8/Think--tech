11070fcee1e1c678df548f57365d95263154ba73
olamiji-8 <olamijisotomi@gmail.com>
2023-03-30 07:34:42 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import "./Testimony.css"
+import About from "./Component/About/About";
+import Courses from "./Component/Courses/Courses";
+import Footer from "./Component/Footer/Footer";
+import Home from "./Component/Home/Home";
+import Sales from "./Component/Sales/Sales";
+import Schedule from "./Component/Schedule/Schedule";
+import Register from "./Register/Register";
+import Question from "./Question/Question";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <Schedule />
+      <About />
+      <Courses />
+      {/* <Testimony /> */}
+      <Register/>
+      <Sales />
+      <Question />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
